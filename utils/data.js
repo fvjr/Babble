e58f12783e7aace412f18c02efe8f1931a58f155
Fernando Vasquez <vasquezjrf@gmail.com>
2022-04-06 20:11:00 -0400

    updated thought controllers, seed data, and routes

--- utils/data.js
+++ utils/data.js
@@ -5,4 +5,12 @@ const users = [
   },
 ];
 
-module.exports = { users };
+const thoughts = [
+  {
+    thoughtText: "test thought text1",
+    username: "testName1",
+    userId: "624e257e61497f8da7ef2324",
+  },
+];
+
+module.exports = { users, thoughts };
--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require("../config/connection");
-const { User } = require("../models");
-const { users } = require("./data");
+const { User, Thought } = require("../models");
+const { users, thoughts } = require("./data");
 
 connection.on("error", (err) => err);
 
@@ -10,11 +10,19 @@ connection.once("open", async () => {
   //drop existing users
   await User.deleteMany({});
 
+  //drop existing thoughts
+  await Thought.deleteMany({});
+
   //empty array to hold users
 
   // const userArray = [];
 
+  //add users to the collection and await the results
   await User.collection.insertMany(users);
+
+  //add thoughts to the collection and await the results
+  await Thought.collection.insertMany(thoughts);
+
   console.table(users);
   console.info("Seeding complete.");
   process.exit(0);
